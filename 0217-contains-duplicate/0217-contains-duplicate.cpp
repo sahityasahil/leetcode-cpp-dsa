5f133a4a22717475f6008575701533127f10f173
Sahitya Sahil <96693115+sahityasahil@users.noreply.github.com>
2025-06-14 20:15:46 +0530

    Time: 47 ms (70.01%), Space: 90.9 MB (30.89%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -3,15 +3,9 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         int n = nums.size();
         unordered_map<int, int> m;
-
         for (int i=0; i<n; i++) {
-            m[nums[i]]++;    
-        }
-
-        for (const auto& e : m) {
-            int key = e.first;
-            int value = e.second;
-            if (value > 1) return true;
+            if(m.find(nums[i]) != m.end()) return true;
+            else m[nums[i]] = 1;
         }
         return false;
     }
