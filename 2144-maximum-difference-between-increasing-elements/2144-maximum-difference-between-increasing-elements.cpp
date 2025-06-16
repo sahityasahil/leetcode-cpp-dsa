36d16e3f4b5489767f2951424a37e4a6e34ec74c
Sahitya Sahil <96693115+sahityasahil@users.noreply.github.com>
2025-06-16 11:03:58 +0530

    Time: 0 ms (100%), Space: 11.9 MB (71.76%) - LeetHub

--- 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.cpp
+++ 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     int maximumDifference(vector<int>& nums) {
-        int n = nums.size();
-        int maxi = -1;
-        for (int i=0; i<n; i++) {
-            for (int j=i+1; j<n; j++) {
-                if (nums[i] < nums[j]) {
-                    maxi = max((nums[j]-nums[i]), maxi);
-                }
+        int diff = -1;
+        int mn = nums[0];
+
+        for(int i=1;i<nums.size();i++){
+            if(nums[i] > mn){
+                // check profit at every possible big day
+                diff = max({nums[i] - mn, diff});
             }
+
+            // check if we can be able to buy on lesser amount day
+            if(nums[i] < mn){mn = nums[i];}
         }
-        return maxi;
+
+        return diff;
     }
 };
