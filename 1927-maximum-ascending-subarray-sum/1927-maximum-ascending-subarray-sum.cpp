c598366c26eca1116d67a6523e86c6c2b51178d7
Sahitya Sahil <96693115+sahityasahil@users.noreply.github.com>
2025-06-20 15:45:11 +0530

    Time: 0 ms (100%), Space: 11 MB (77.48%) - LeetHub

--- 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.cpp
+++ 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.cpp
@@ -3,7 +3,7 @@ public:
     int maxAscendingSum(vector<int>& nums) {
         int n = nums.size();
         int maxSum = 0, sum = nums[0]; // global sum
-        maxSum = max(sum, maxSum);
+        maxSum = max(sum, maxSum); // if the first number is the answer itself (eg. 100 10 1)
         for (int i=1; i<n; i++) {
             if (nums[i] > nums[i-1]) sum += nums[i];
             else sum = nums[i];
