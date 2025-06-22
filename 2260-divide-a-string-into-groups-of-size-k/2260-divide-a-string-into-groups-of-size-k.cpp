f94617dde078820169c53ab6492b244e6942cce9
Sahitya Sahil <96693115+sahityasahil@users.noreply.github.com>
2025-06-22 14:52:41 +0530

    Time: 0 ms (100%), Space: 9.6 MB (93.73%) - LeetHub

--- 2260-divide-a-string-into-groups-of-size-k/2260-divide-a-string-into-groups-of-size-k.cpp
+++ 2260-divide-a-string-into-groups-of-size-k/2260-divide-a-string-into-groups-of-size-k.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     vector<string> divideString(string s, int k, char fill) {
+        vector<string> ans;
+        int n = s.length();
+    
+        int padding = (k - (n % k)) % k;  // Handles case when n%k == 0
+        s.append(padding, fill);
         
+        for (int i=0; i<n; i+=k) {
+            ans.push_back(s.substr(i, k));
+        }
+        return ans;
     }
 };
