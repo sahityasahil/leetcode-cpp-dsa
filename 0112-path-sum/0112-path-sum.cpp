8136c426f0762fb54eebe5918ea8e6afc951c8ee
Sahitya Sahil <96693115+sahityasahil@users.noreply.github.com>
2025-07-08 10:13:02 +0530

    Time: 0 ms (100%), Space: 21.5 MB (33.74%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -13,7 +13,7 @@ class Solution {
 public:
     bool hasPathSum(TreeNode* root, int targetSum) {
         if (root == NULL) return false;
-        if (root->right == NULL && root->left == NULL) return (targetSum == root->val);
+        if (root->right == NULL && root->left == NULL) return (targetSum == root->val); // check if the leaf node's value is exactly equal to the value of leaf node or not, if yes then the path exists
         return hasPathSum(root->left, targetSum-root->val) || hasPathSum(root->right, targetSum-root->val);
     }
 };
