5077ce8b58e6c4ba55a379aa1bb9998c5199a732
Sahitya Sahil <96693115+sahityasahil@users.noreply.github.com>
2025-12-10 21:15:06 +0530

    Time: 25 ms (71.58%), Space: 17.5 MB (44.77%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,29 +1,37 @@
 class Solution {
 public:
-    int m, n;
-    void dfs(int r, int c, vector<vector<char>>& grid, vector<vector<bool>>& visited){
-        if (r < 0 || r >= m || c < 0 || c >= n || visited[r][c] == true || grid[r][c] == '0'){
+    void dfs(int row, int col, vector<vector<char>>& grid,
+             vector<vector<int>>& vis, int m, int n) {
+        
+        if (row < 0 || col < 0 || row >= m || col >= n || 
+            vis[row][col] == 1 || grid[row][col] == '0') {
             return;
         }
-        visited[r][c] = true;
-        dfs(r+1, c, grid, visited);
-        dfs(r, c+1, grid, visited);
-        dfs(r-1, c, grid, visited);
-        dfs(r, c-1, grid, visited);
+        
+        vis[row][col] = 1;
+
+        dfs(row+1, col, grid, vis, m, n);
+        dfs(row-1, col, grid, vis, m, n);
+        dfs(row, col+1, grid, vis, m, n);
+        dfs(row, col-1, grid, vis, m, n);
     }
+
     int numIslands(vector<vector<char>>& grid) {
-        m = grid.size();
-        n = grid[0].size();
-        int res = 0;
-        vector<vector<bool>> visited(m, vector<bool>(n));
-        for (int i=0; i<m; i++){
-            for (int j = 0; j <n; j++){
-                if(grid[i][j] == '1' && visited[i][j] == false){
-                    dfs(i, j, grid, visited);
-                    res++;
-                } 
+        int m = grid.size();
+        int n = grid[0].size();
+        int ans = 0;
+
+        vector<vector<int>> vis(m, vector<int>(n, 0));
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1' && vis[i][j] == 0) {
+                    dfs(i, j, grid, vis, m, n);
+                    ans++;
+                }
             }
         }
-        return res;
+
+        return ans;
     }
 };
